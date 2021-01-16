3e1d465af6fd730898e87211838c37e3736b2266
Nick Taras <nicktaras@hotmail.co.uk>
2021-01-16 15:49:19 +1100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	let newItem = "";
+
+	// Example todo:
+	// { text: "Write my first post", status: true }
+	let todoList = localStorage.getItem("todos")
+		? JSON.parse(localStorage.getItem("todos"))
+		: [];
+
+	function addToList() {
+		todoList = [...todoList, { text: newItem, status: false }];
+		newItem = "";
+		localStorage.setItem("todos", JSON.stringify(todoList));
+	}
+
+	function removeFromList(index) {
+		todoList.splice(index, 1);
+		todoList = todoList;
+		localStorage.setItem("todos", JSON.stringify(todoList));
+	}
+</script>
+
+<style>
+	.checked {
+		text-decoration: line-through;
+	}
+</style>
+
+<form on:submit|preventDefault={addToList}>
+	<input bind:value={newItem} type="text" placeholder="new todo item.." />
+	<button type="submit"> Submit </button>
+</form>
+
+{#each todoList as item, index}
+	<input bind:checked={item.status} type="checkbox" />
+	<span class:checked={item.status}>{item.text}</span>
+	<span style="cursor: pointer" on:click={() => removeFromList(index)}>🗑</span>
+	<br />
+{/each}
